60764d6ed38109e3e0271c48a631e26957624ea1
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-07 22:07:28 +0900

    tour of go /flowcontrol/7

--- go/index.go
+++ go/index.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	forLoop()
 	packages()
 	imports()
 	exportedNames()
@@ -29,6 +28,84 @@ func main() {
 	constants()
 	numericConstants()
 	pointer()
+	forLoop()
+	forContinued()
+	while()
+	forever()
+	ifFunc()
+	ifWithShortStatement()
+	ifAndElse()
+}
+
+func ifAndElse() {
+	title := "if and else"
+	fmt.Println("---", title, "---")
+	fmt.Println(
+		pow2(3, 2, 10),
+		pow2(3, 3, 20),
+
+	)
+	
+}
+func pow2(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+		} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+func ifWithShortStatement() {
+	title := "if with a short statement"
+	fmt.Println("---", title, "---")
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+func ifFunc() {
+	title := "if"
+	fmt.Println("---", title, "---")
+	fmt.Println("sqrt(2): ", sqrt(2))
+	fmt.Println("sqrt(-4)", sqrt(-4))
+}
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+func forever() {
+	title := "forever"
+	fmt.Println("---", title, "---")
+	// for {}
+}
+
+func while() {
+	title := "while"
+	fmt.Println("---", title, "---")
+	sum := 3
+	for sum < 1000 {
+		sum += sum
+		fmt.Println(sum)
+	}
+}
+
+func forContinued() {
+	var title string = "for continued"
+	fmt.Println("---", title, "---")
+	sum := 1
+	for ; sum < 1000; {
+		fmt.Println(sum)
+		sum += sum
+	}
 }
 
 func forLoop() {
