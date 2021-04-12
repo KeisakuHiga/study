643a95107c3a9d7148ff48c0fad3254370290c29
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-12 22:17:44 +0900

    structsLiterals

--- go/index.go
+++ go/index.go
@@ -48,8 +48,26 @@ func main() {
 	structs()
 	structsFields()
 	structsPointers()
+	structsLiterals()
 }
 
+func structsLiterals() {
+	title := "structsLiterals"
+	fmt.Println("---", title, "---")
+	type Vertex struct {
+		X, Y int
+	}
+	var (
+		v1 = Vertex{1, 2}		// has type Vertex
+		v2 = Vertex{X: 1}		// Y: 0 is implicit
+		v3 = Vertex{}				// X: 0, Y: 0
+		p  = &Vertex{1, 2}	// has type *Vertex
+	)
+	fmt.Println("v1: ", v1)
+	fmt.Println("v2: ", v2)
+	fmt.Println("v3: ", v3)
+	fmt.Println("p: ", p)
+}
 func structsPointers() {
 	title := "structsPointers"
 	fmt.Println("---", title, "---")
