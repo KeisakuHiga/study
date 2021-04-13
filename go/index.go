2b3c511c7214626ccaca5bbc3fb73dbb73e78640
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-13 21:07:12 +0900

    sliceLiterals

--- go/index.go
+++ go/index.go
@@ -52,8 +52,32 @@ func main() {
 	arrayFunc()
 	slices()
 	slicesPointers()
+	sliceLiterals()
 }
 
+func sliceLiterals() {
+	title := "sliceLiterals"
+	fmt.Println("---", title, "---")
+	fmt.Println(`A slice literal is like an array literal without the length.`)
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("q: ", q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println("r: ", r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println("s: ", s)
+}
 func slicesPointers() {
 	title := "slicesPointers"
 	fmt.Println("---", title, "---")
