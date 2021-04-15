aec426f05e2683ca96fe587c860a2cc46f9399ad
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-16 08:08:40 +0900

    mutating maps

--- go/index.go
+++ go/index.go
@@ -66,6 +66,22 @@ func main() {
 	rangeFunc2()
 	mapsFunc()
 	mapLiterals()
+	mutatingMaps()
+}
+
+func mutatingMaps() {
+	title := "mutating maps"
+	fmt.Println("---", title, "---")
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
 
 func mapLiterals() {
