f6fa59ccd59bb8b9621e5cd24bf249fce03bbfeb
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-15 12:45:28 +0900

    append

--- go/index.go
+++ go/index.go
@@ -59,6 +59,26 @@ func main() {
 	nilSlices()
 	makingSlices()
 	slicesOfSlice()
+	appendFunc()
+}
+
+func appendFunc() {
+	title := "appendFunc"
+	fmt.Println("---", title, "---")
+	var s []int
+	printSlice(s)
+	
+	fmt.Println("append works on nil slices.")
+	s = append(s, 0)
+	printSlice(s)
+	
+	fmt.Println("The slice grows as needed.")
+	s = append(s, 1)
+	printSlice(s)
+
+	fmt.Println("We can add more than one element at a time.")
+	s = append(s, 2, 3, 4, 5)
+	printSlice(s)	
 }
 
 func slicesOfSlice() {
