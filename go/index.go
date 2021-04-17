91983d0f5cdc209835815ff3bc5c6eae26283112
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-17 12:29:27 +0900

    function values

--- go/index.go
+++ go/index.go
@@ -67,6 +67,23 @@ func main() {
 	mapsFunc()
 	mapLiterals()
 	mutatingMaps()
+	functionValues()
+}
+
+func functionValues() {
+	title := "function values"
+	fmt.Println("---", title, "---")
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12))
+
+	fmt.Println(hypot)
+	fmt.Println(compute(hypot))
+	fmt.Println(math.Pow)
+}
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
 }
 
 func mutatingMaps() {
