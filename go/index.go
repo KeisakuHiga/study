15b5b07af7adcbbb6a6506d7a9dbe95050170b47
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-19 22:02:42 +0900

    make slices 2

--- go/index.go
+++ go/index.go
@@ -69,6 +69,25 @@ func main() {
 	mutatingMaps()
 	functionValues()
 	functionClosures()
+	makeSlices2()
+}
+
+func makeSlices2() {
+	title := "make slices 2"
+	fmt.Println("---", title, "---")
+	c := make([]int, 5)
+	for i := 0; i < 5; i++ {
+		c = append(c, i)
+		fmt.Println(c)
+	}
+	fmt.Println(c)
+
+	d := make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		d = append(d, i)
+		fmt.Println(d)
+	}
+	fmt.Println(d)
 }
 
 func functionClosures() {
