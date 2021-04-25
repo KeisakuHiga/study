79f51355cdde8182a6d5610d43633879a5e3ed1a
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-25 16:19:19 +0900

    interface and dug typing

--- go/index.go
+++ go/index.go
@@ -90,6 +90,39 @@ func main() {
 	methodsFuncs()
 	method()
 	methodNonStruct()
+	interfaceFunc()
+}
+func interfaceFunc() {
+	title := "interfaceFunc"
+	fmt.Println("---", title, "---")
+	var mike Human = &Person{"Mike"}
+	var x Human = &Person{"X"}
+	// var dog Dog = Dog{"dog"}
+	// mike.Say()
+	DriveCar(mike)
+	DriveCar(x)
+	// DriveCar(dog)
+}
+type Human interface {
+	Say() string
+}
+type Person struct {
+	Name string
+}
+type Dog struct {
+	Name string
+}
+func (p *Person) Say() string{
+	p.Name = "Mr." + p.Name
+	fmt.Println(p.Name)
+	return p.Name
+}
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("Run")
+	}else{
+		fmt.Println("Get out")
+	}
 }
 func methodNonStruct() {
 	title := "methodNonStruct"
