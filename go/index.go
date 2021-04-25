2870fd6bca7edeba8a3f26951b859cda82f034c9
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-25 17:23:26 +0900

    practice3: method and stringer

--- go/index.go
+++ go/index.go
@@ -94,6 +94,20 @@ func main() {
 	typeAssertion()
 	stringer()
 	customError()
+	practice3()
+}
+func practice3() {
+	title := "practice3"
+	fmt.Println("---", title, "---")
+	v := Vertex{3, 4}
+	fmt.Println(v.Plus())
+	fmt.Println(v)
+}
+func (v Vertex) Plus() int {
+	return v.x + v.y
+}
+func (v Vertex) String() string {
+	return fmt.Sprintf("X is %v Y is %v", v.x, v.y)
 }
 func customError() {
 	title := "customError"
