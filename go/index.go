56d7fe0b5815bf8092e2c4e5f6db5143b1191597
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-26 08:22:01 +0900

    channelWithRangeAndClose

--- go/index.go
+++ go/index.go
@@ -99,6 +99,25 @@ func main() {
 	goroutine()
 	channel()
 	bufferedChannel()
+	channelWithRangeAndClose()
+}
+func channelWithRangeAndClose() {
+	title := "channelWithRangeAndClose"
+	fmt.Println("---", title, "---")
+	s := []int{1,2,3,4,5}
+	c := make(chan int, len(s))
+	go goroutine3(s, c)
+	for i := range c{
+		fmt.Println(i)
+	}
+}
+func goroutine3(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
 }
 func bufferedChannel() {
 	title := "bufferedChannel"
