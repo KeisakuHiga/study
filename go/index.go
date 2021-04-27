719034a55529652667593772110f113a3396766c
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-27 23:37:12 +0900

    producerAndConsumer

--- go/index.go
+++ go/index.go
@@ -100,7 +100,39 @@ func main() {
 	channel()
 	bufferedChannel()
 	channelWithRangeAndClose()
+	producerAndConsumer()
 }
+
+func producerAndConsumer() {
+	title := "producerAndConsumer"
+	fmt.Println("---", title, "---")
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	// Producer
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go producer(ch, i)
+	}
+
+	// Consumer
+	go consumer(ch, &wg)
+	wg.Wait()
+	close(ch)
+}
+func producer(ch chan int, i int) {
+	ch <- i * 2
+}
+
+func consumer(ch chan int, wg *sync.WaitGroup) {
+	for i := range ch {
+		func () {
+			defer wg.Done()
+			fmt.Println("process", i * 1000)
+		}()
+	}
+}
+
 func channelWithRangeAndClose() {
 	title := "channelWithRangeAndClose"
 	fmt.Println("---", title, "---")
