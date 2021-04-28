911535b4a3f16d9c2e048031efc75de37de4a4e4
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-04-28 19:00:25 +0900

    fanOutFanIn

--- go/index.go
+++ go/index.go
@@ -101,6 +101,41 @@ func main() {
 	bufferedChannel()
 	channelWithRangeAndClose()
 	producerAndConsumer()
+	fanOutFanIn()
+}
+
+func fanOutFanIn() {
+	title := "fanOutFanIn"
+	fmt.Println("---", title, "---")
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
+
+	go producer2(first)
+	go multi2(first, second)
+	go multi4(second, third)
+
+	for result := range third {
+		fmt.Println(result)
+	}
+}
+func producer2(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
+}
+func multi2(first chan int, second chan int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
+	}
+}
+func multi4(second chan int, third chan int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 4
+	}
 }
 
 func producerAndConsumer() {
