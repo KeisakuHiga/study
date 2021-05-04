981feb113d4d275a687d539e9005feb5df91aad6
KeisakuHiga <khiga2943@gmail.com>
2021-05-04 22:00:29 +0900

    use dotenv to mange env variables

--- ts/node_project/src/app.ts
+++ ts/node_project/src/app.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+const port = process.env.PORT;
+app.get("/", (_, res) => {
+  res.send("The sedulous hyena ate the antelope!");
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
