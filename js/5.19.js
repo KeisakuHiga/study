a2258853d8c000240939fe78211bcf92d8738110
KeisakuHiga <khiga2943@gmail.com>
2021-05-07 20:19:20 +0900

    prototype chain

---
+++ js/5.19.js
@@ -0,0 +1,29 @@
+var Animal = function () { }
+Animal.prototype = {
+  walk: function () {
+    console.log('toko...toko...');
+  }
+}
+
+var Dog = function () {
+  // 継承してる
+  Animal.call(this)
+}
+// DogオブジェクトのプロトタイプとしてAnimalオブエクトのインスタンスをセット
+Dog.prototype = new Animal()
+Dog.prototype.bark = function () {
+  console.log('one! one!')
+}
+
+var d = new Dog()
+// Animalオブジェクトのprototypeにセットしたwalk()がDogインスタンスからでも呼び出せる
+// プロパティ・メソッドの検索順番
+// Dogインスタンス自身のメンバー
+//  -> Dogオブジェクトのprototype
+//  -> Animalインスタンスのメンバー
+//  -> Animalオブジェクトのprototype
+//  -> （最上位）Object.prototype
+d.walk()
+d.bark()
+
+
