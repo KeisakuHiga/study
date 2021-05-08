0af8cfcaebfc9d047d7c07ac0eb344bee8ba9fc4
KeisakuHiga <khiga2943@gmail.com>
2021-05-08 21:57:51 +0900

    getter / setter

---
+++ js/5.26.js
@@ -0,0 +1,50 @@
+function Triangle() {
+  // private members
+  var _base
+  var _height
+
+  // define 'base' private member
+  Object.defineProperty(
+    this,
+    'base',
+    {
+      get: function () {
+        return _base
+      },
+      set: function (base) {
+        if (typeof base === 'number' && base > 0) {
+          _base = base
+        }
+      }
+    }
+  )
+
+  // define 'height' private member
+  Object.defineProperty(
+    this,
+    'height',
+    {
+      get: function () {
+        return _height
+      },
+      set: function (height) {
+        if (typeof height === 'number' && height > 0) {
+          _height = height
+        }
+      }
+    }
+  )
+}
+// basic prototype methods which won't access private members
+Triangle.prototype.getArea = function () {
+  return this.base * this.height / 2
+}
+
+var t = new Triangle()
+t.base = 10
+t.height = 5
+console.log(t.base)  //10
+console.log(t.height)  //5
+console.log(t.getArea())  //25
+
+
